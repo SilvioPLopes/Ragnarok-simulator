a12037ce46882090aebbd8ce0b88359176220d06
silvio <silviopln@hotmail.com>
2026-03-12 21:39:12 -0300

    feat: add findDistinctJobClasses() to SkillTreeRepository

--- src/main/java/com/ragnarok/infrastructure/persistence/SkillTreeRepository.java
+++ src/main/java/com/ragnarok/infrastructure/persistence/SkillTreeRepository.java
@@ -1,9 +1,13 @@
 package com.ragnarok.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SkillTreeRepository extends JpaRepository<SkillTreeEntity, Integer> {
     List<SkillTreeEntity> findByJobClassIgnoreCase(String jobClass);
     List<SkillTreeEntity> findByJobClassIgnoreCaseAndSkillId(String jobClass, String skillId);
+
+    @Query("SELECT DISTINCT UPPER(s.jobClass) FROM SkillTreeEntity s")
+    List<String> findDistinctJobClasses();
 }
